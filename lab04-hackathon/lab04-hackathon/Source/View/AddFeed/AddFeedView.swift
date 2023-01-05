0e81516c6fa74721cf21f9394e9c7bf8357af723
Da01002 <80445363+Da01002@users.noreply.github.com>
2023-01-05 22:50:24 +0900

    캐릭터, 캐릭터 백그라운드 변경

--- lab04-hackathon/lab04-hackathon/Source/View/AddFeed/AddFeedView.swift
+++ lab04-hackathon/lab04-hackathon/Source/View/AddFeed/AddFeedView.swift
@@ -20,124 +20,111 @@ struct AddFeedView: View {
     @State private var selectedImageData: Data? = nil
     
     var body: some View {
-        VStack {
-            Text("새 게시물")
-                .font(.cafeBody)
-            //MARK: - 사용자의 그림
-            PhotosPicker(
-                selection: $selectedImage,
-                matching: .images,
-                photoLibrary: .shared()) {
-                    if selectedImageData == nil {
-                        Rectangle()
-                            .foregroundColor(Color(red: 255/255, green: 225/255, blue: 225/255))
-                            .frame(width:UIScreen.main.bounds.size.width, height:UIScreen.main.bounds.size.width)
-                            .overlay(
-                                Image("imageChoice")
-                                    .resizable()
-                                //.shadow(color: .white, radius: 5)
-                                    .frame(width:UIScreen.main.bounds.size.width*0.6, height:UIScreen.main.bounds.size.width*0.6)
-                            )
-                    } else {
-                        if let selectedImageData,
-                           let uiImage = UIImage(data: selectedImageData) {
-                            Image(uiImage: uiImage)
-                                .resizable()
+        ScrollView {
+            VStack {
+                Text("새 게시물")
+                    .font(.cafeHeadline2)
+                //MARK: - 사용자의 그림
+                PhotosPicker(
+                    selection: $selectedImage,
+                    matching: .images,
+                    photoLibrary: .shared()) {
+                        if selectedImageData == nil {
+                            Rectangle()
+                                .foregroundColor(Color(red: 236/255, green: 232/255, blue: 221/255))
                                 .frame(width:UIScreen.main.bounds.size.width, height:UIScreen.main.bounds.size.width)
+                                .overlay(
+                                    Image("imageChoice")
+                                        .resizable()
+                                    //.shadow(color: .white, radius: 5)
+                                        .frame(width:UIScreen.main.bounds.size.width*0.6, height:UIScreen.main.bounds.size.width*0.6)
+                                )
+                        } else {
+                            if let selectedImageData,
+                               let uiImage = UIImage(data: selectedImageData) {
+                                Image(uiImage: uiImage)
+                                    .resizable()
+                                    .frame(width:UIScreen.main.bounds.size.width, height:UIScreen.main.bounds.size.width)
+                            }
                         }
                     }
-                }
-                .onChange(of: selectedImage) { newItem in
-                    Task {
-                        // Retrieve selected asset in the form of Data
-                        if let data = try? await newItem?.loadTransferable(type: Data.self) {
-                            selectedImageData = data
+                    .onChange(of: selectedImage) { newItem in
+                        Task {
+                            // Retrieve selected asset in the form of Data
+                            if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                                selectedImageData = data
+                            }
                         }
                     }
-                }
-            
-            
-            // MARK: - 카테고리
-            VStack {
-                HStack {
-                    Text("카테고리")
-                        .font(.cafeBody)
-                    Spacer()
-                    
-                    
-                    Menu{
-                        ForEach(Array(drawingCategory.enumerated()), id: \.offset){index, category in
-                            Button(action: {
-                                self.index = index
-                                self.currentCategory = category
-                            }, label: {
-                                HStack{
-                                    Text(category)
-                                    if currentCategory == drawingCategory[index]{
-                                        Image(systemName: "checkmark")
+                
+                
+                // MARK: - 카테고리
+                VStack {
+                    HStack {
+                        Text("카테고리")
+                            .font(.cafeHeadline2)
+                        Spacer()
+                        // 카테고리 피커(메뉴)
+                        Menu{
+                            ForEach(Array(drawingCategory.enumerated()), id: \.offset){index, category in
+                                Button(action: {
+                                    self.index = index
+                                    self.currentCategory = category
+                                }, label: {
+                                    HStack{
+                                        Text(category)
+                                        if currentCategory == drawingCategory[index]{
+                                            Image(systemName: "checkmark")
+                                        }
                                     }
-                                }
-                            })
-                        }
-                    } label: {
-                        HStack {
-                            Text(drawingCategory[index])
-                                .foregroundColor(.black)
-                                .font(.cafeBody)
-                                .frame(width: UIScreen.main.bounds.width*0.25)
-                            Image(systemName: "chevron.down")
-                                .foregroundColor(.black)
-                        }
-                        
+                                })
+                            }
+                        } label: {
+                            HStack {
+                                Text(drawingCategory[index])
+                                    .foregroundColor(.black)
+                                    .font(.cafeHeadline2)
+                                    .frame(width: UIScreen.main.bounds.width*0.25)
+                                Image(systemName: "chevron.down")
+                                    .foregroundColor(.black)
+                            }
+                            
+                        }.animation(nil)
                     }
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-//                    Picker(selection: $currentCategory, label: Text("Picker")) {
-//                        ForEach(0..<drawingCategory.count) {
-//                            Text(drawingCategory[$0])
-//                        }
-//                    }
-//                    .pickerStyle(.menu)
-//                    .tint(.black)
+                    Divider()
                 }
-                Divider()
-            }
-            .padding()
-            
-            // MARK: - 문구 입력
-            VStack {
-                HStack {
-                    Text("문구입력")
-                        .font(.cafeBody)
-                    Spacer()
+                .padding()
+                
+                // MARK: - 문구 입력
+                VStack {
+                    HStack {
+                        Text("문구입력")
+                            .font(.cafeHeadline2)
+                        Spacer()
+                    }
+                    TextEditor(text: $drawingDescription)
+                        .font(.cafeHeadline2)
+                        .overlay (
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(lineWidth: 0.3)
+                                .foregroundColor(.gray)
+                        )
+                        
                 }
-                TextEditor(text: $drawingDescription)
-                    .font(.cafeBody)
+                .padding([.leading, .trailing])
+                Spacer()
+                
+                //테스트 버튼
+                //            Button(action: {
+                //                let imageURL = UUID().uuidString
+                //                feedStore.uploadImage(image: selectedImageData, imageURL: imageURL)
+                //                feedStore.create(Feed(feedId: <#T##String#>, userId: <#T##String#>, title: <#T##String#>, imageURL: <#T##String#>, description: <#T##String#>, category: <#T##String#>, userName: <#T##String#>, date: <#T##Date#>))
+                //
+                //            }) {
+                //                Text("스토리지에 사진넣기")
+                //            }
                 
             }
-            .padding([.leading, .trailing])
-            Spacer()
-            
-            //테스트 버튼
-            //            Button(action: {
-            //                let imageURL = UUID().uuidString
-            //                feedStore.uploadImage(image: selectedImageData, imageURL: imageURL)
-            //                feedStore.create(Feed(feedId: <#T##String#>, userId: <#T##String#>, title: <#T##String#>, imageURL: <#T##String#>, description: <#T##String#>, category: <#T##String#>, userName: <#T##String#>, date: <#T##Date#>))
-            //
-            //            }) {
-            //                Text("스토리지에 사진넣기")
-            //            }
-            
         }
         .toolbar {
             ToolbarItem(placement: .navigationBarLeading) {
