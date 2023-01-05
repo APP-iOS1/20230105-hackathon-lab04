c28164bea5101fd494ec84b97c1744ee87c73336
Da01002 <80445363+Da01002@users.noreply.github.com>
2023-01-05 16:36:23 +0900

    [feat] AddFeedView 추가

--- lab04-hackathon/lab04-hackathon/Source/View/AddFeed/AddFeedView.swift
+++ lab04-hackathon/lab04-hackathon/Source/View/AddFeed/AddFeedView.swift
@@ -1,15 +1,64 @@
 //
-//  AddFeedView.swift
-//  lab04-hackathon
+//  AddfeedView.swift
+//  hackathonPreview
 //
-//  Created by MacBook on 2023/01/05.
+//  Created by 장다영 on 2023/01/05.
 //
 
 import SwiftUI
 
 struct AddFeedView: View {
+    
+    var drawingCategory = ["풍경 그림", "캐릭터 그림", "인물 그림", "동물 그림"]
+    @State private var currentCategory : Int = 0
+    @State private var drawingDescription : String = ""
+    
     var body: some View {
-        Text("AddFeedView, World!")
+        VStack {
+            Text("새 게시물")
+            //MARK: - 사용자의 그림
+            Rectangle()
+                .frame(width:UIScreen.main.bounds.size.width, height:UIScreen.main.bounds.size.width)
+            
+            
+            // MARK: - 카테고리
+            VStack {
+                HStack {
+                    Text("카테고리")
+                    Spacer()
+                    Picker(selection: $currentCategory, label: Text("Picker")) {
+                        ForEach(0..<drawingCategory.count) {
+                            Text(drawingCategory[$0])
+                        }
+                    }
+                    .pickerStyle(.menu)
+                    .tint(.black)
+                }
+                Divider()
+            }
+            .padding()
+            
+            // MARK: - 문구 입력
+            VStack {
+                HStack {
+                    Text("문구입력")
+                    Spacer()
+                }
+                TextEditor(text: $drawingDescription)
+
+            }
+            .padding([.leading, .trailing])
+            Spacer()
+            
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button("완료") {
+                    
+                }
+            }
+        }
+        .background(Color(red: 252/255, green: 251/255, blue: 245/255))
     }
 }
 
@@ -18,3 +67,4 @@ struct AddFeedView_Previews: PreviewProvider {
         AddFeedView()
     }
 }
+
