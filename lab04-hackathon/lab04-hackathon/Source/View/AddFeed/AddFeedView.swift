9574a371a967248ec822e2bbbac0db59b0baab6a
mrgroomy <konakona0106@gmail.com>
2023-01-05 16:54:37 +0900

    add_AddFeedView_PhotosPicker
    AddFeedView- 포토피커 추가작업

--- lab04-hackathon/lab04-hackathon/Source/View/AddFeed/AddFeedView.swift
+++ lab04-hackathon/lab04-hackathon/Source/View/AddFeed/AddFeedView.swift
@@ -6,19 +6,45 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct AddFeedView: View {
     
     var drawingCategory = ["풍경 그림", "캐릭터 그림", "인물 그림", "동물 그림"]
     @State private var currentCategory : Int = 0
     @State private var drawingDescription : String = ""
+    @State private var selectedImage: PhotosPickerItem? = nil
+    @State private var selectedImageData: Data? = nil
     
     var body: some View {
         VStack {
             Text("새 게시물")
             //MARK: - 사용자의 그림
-            Rectangle()
-                .frame(width:UIScreen.main.bounds.size.width, height:UIScreen.main.bounds.size.width)
+            PhotosPicker(
+                selection: $selectedImage,
+                matching: .images,
+                photoLibrary: .shared()) {
+                    if selectedImageData == nil {
+                        Text("이미지를 선택해주세요")
+                            .font(.largeTitle)
+                            .frame(width:UIScreen.main.bounds.size.width, height:UIScreen.main.bounds.size.width)
+                    } else {
+                        if let selectedImageData,
+                           let uiImage = UIImage(data: selectedImageData) {
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .frame(width:UIScreen.main.bounds.size.width, height:UIScreen.main.bounds.size.width)
+                        }
+                    }
+                }
+                .onChange(of: selectedImage) { newItem in
+                    Task {
+                        // Retrieve selected asset in the form of Data
+                        if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                            selectedImageData = data
+                        }
+                    }
+                }
             
             
             // MARK: - 카테고리
