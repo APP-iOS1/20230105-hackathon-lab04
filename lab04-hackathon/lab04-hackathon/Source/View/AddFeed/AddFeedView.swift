907cd3f8e0874cb045799cfd6548bac0ef3a93d6
Da01002 <80445363+Da01002@users.noreply.github.com>
2023-01-05 22:30:32 +0900

    picker 변경

--- lab04-hackathon/lab04-hackathon/Source/View/AddFeed/AddFeedView.swift
+++ lab04-hackathon/lab04-hackathon/Source/View/AddFeed/AddFeedView.swift
@@ -12,8 +12,9 @@ struct AddFeedView: View {
     
     @ObservedObject var feedStore: FeedStore = FeedStore()
     
-    var drawingCategory = ["풍경 그림", "캐릭터 그림", "인물 그림", "동물 그림"]
-    @State private var currentCategory : Int = 0
+    var drawingCategory = ["캐릭터 그림", "인물 그림", "동물 그림", "풍경 그림"]
+    @State private var currentCategory : String = "풍경 그림"
+    @State private var index = 0
     @State private var drawingDescription : String = ""
     @State private var selectedImage: PhotosPickerItem? = nil
     @State private var selectedImageData: Data? = nil
@@ -21,15 +22,22 @@ struct AddFeedView: View {
     var body: some View {
         VStack {
             Text("새 게시물")
+                .font(.cafeBody)
             //MARK: - 사용자의 그림
             PhotosPicker(
                 selection: $selectedImage,
                 matching: .images,
                 photoLibrary: .shared()) {
                     if selectedImageData == nil {
-                        Text("이미지를 선택해주세요")
-                            .font(.largeTitle)
+                        Rectangle()
+                            .foregroundColor(Color(red: 255/255, green: 225/255, blue: 225/255))
                             .frame(width:UIScreen.main.bounds.size.width, height:UIScreen.main.bounds.size.width)
+                            .overlay(
+                                Image("imageChoice")
+                                    .resizable()
+                                //.shadow(color: .white, radius: 5)
+                                    .frame(width:UIScreen.main.bounds.size.width*0.6, height:UIScreen.main.bounds.size.width*0.6)
+                            )
                     } else {
                         if let selectedImageData,
                            let uiImage = UIImage(data: selectedImageData) {
@@ -53,15 +61,54 @@ struct AddFeedView: View {
             VStack {
                 HStack {
                     Text("카테고리")
-                        .font(.custom("Cafe24Ssurround", size: 20))
+                        .font(.cafeBody)
                     Spacer()
-                    Picker(selection: $currentCategory, label: Text("Picker")) {
-                        ForEach(0..<drawingCategory.count) {
-                            Text(drawingCategory[$0])
+                    
+                    
+                    Menu{
+                        ForEach(Array(drawingCategory.enumerated()), id: \.offset){index, category in
+                            Button(action: {
+                                self.index = index
+                                self.currentCategory = category
+                            }, label: {
+                                HStack{
+                                    Text(category)
+                                    if currentCategory == drawingCategory[index]{
+                                        Image(systemName: "checkmark")
+                                    }
+                                }
+                            })
                         }
+                    } label: {
+                        HStack {
+                            Text(drawingCategory[index])
+                                .foregroundColor(.black)
+                                .font(.cafeBody)
+                                .frame(width: UIScreen.main.bounds.width*0.25)
+                            Image(systemName: "chevron.down")
+                                .foregroundColor(.black)
+                        }
+                        
                     }
-                    .pickerStyle(.menu)
-                    .tint(.black)
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+//                    Picker(selection: $currentCategory, label: Text("Picker")) {
+//                        ForEach(0..<drawingCategory.count) {
+//                            Text(drawingCategory[$0])
+//                        }
+//                    }
+//                    .pickerStyle(.menu)
+//                    .tint(.black)
                 }
                 Divider()
             }
@@ -71,25 +118,25 @@ struct AddFeedView: View {
             VStack {
                 HStack {
                     Text("문구입력")
-                        .font(.custom("Cafe24Ssurround", size: 20))
+                        .font(.cafeBody)
                     Spacer()
                 }
                 TextEditor(text: $drawingDescription)
-                    .font(.custom("Cafe24Ssurround", size: 15))
-
+                    .font(.cafeBody)
+                
             }
             .padding([.leading, .trailing])
             Spacer()
             
             //테스트 버튼
-//            Button(action: {
-//                let imageURL = UUID().uuidString
-//                feedStore.uploadImage(image: selectedImageData, imageURL: imageURL)
-//                feedStore.create(Feed(feedId: <#T##String#>, userId: <#T##String#>, title: <#T##String#>, imageURL: <#T##String#>, description: <#T##String#>, category: <#T##String#>, userName: <#T##String#>, date: <#T##Date#>))
-//
-//            }) {
-//                Text("스토리지에 사진넣기")
-//            }
+            //            Button(action: {
+            //                let imageURL = UUID().uuidString
+            //                feedStore.uploadImage(image: selectedImageData, imageURL: imageURL)
+            //                feedStore.create(Feed(feedId: <#T##String#>, userId: <#T##String#>, title: <#T##String#>, imageURL: <#T##String#>, description: <#T##String#>, category: <#T##String#>, userName: <#T##String#>, date: <#T##Date#>))
+            //
+            //            }) {
+            //                Text("스토리지에 사진넣기")
+            //            }
             
         }
         .toolbar {
