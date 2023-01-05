0b3f6af7a0d309e28fd04e783ee15526d152b386
서광현 <68586179+seo-kh@users.noreply.github.com>
2023-01-05 21:10:52 +0900

    Create Font+.swift
    폰트 익스텐션..

---
+++ lab04-hackathon/lab04-hackathon/Configuration/Extensions/Font+.swift
@@ -0,0 +1,11 @@
+// 2023/01/05 created by 서광현.
+
+import SwiftUI
+
+extension Font {
+    static let cafeTitle: Font = .custom("Cafe24Ssurround", size: 28)
+    static let cafeLargeTitle: Font = .custom("Cafe24Ssurround", size: 34)
+    static let cafeHeadline: Font = .custom("Cafe24Ssurround", size: 17)
+    static let cafeBody: Font = .custom("Cafe24SsurroundAir", size: 17)
+    
+}
