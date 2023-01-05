b9fe37ba18ff12532432c24cc4cb2b166b3466aa
mrgroomy <konakona0106@gmail.com>
2023-01-05 20:19:16 +0900

    feat_CanvasView
    [Add] feat_CanvasView 추가

--- lab04-hackathon/lab04-hackathon/Source/View/ContentView.swift
+++ lab04-hackathon/lab04-hackathon/Source/View/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
                 switch selectedTabBar {
                     
                 case .first:
-                    EmptyView()
+                    CanvasView()
                 case .second:
                     EmptyView()
                 case .third:
